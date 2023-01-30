2ab779caabb4b334bcef0185750d2c6f436afc4d
M5c8n31d3r <m.schneider-kleintransporte@web.de>
2023-01-30 20:52:34 +0100

    sum.js

--- js/summary.js
+++ js/summary.js
@@ -1,9 +1,9 @@
 async function initSummary() {
+  await includeHTML();
+  setURL(
+    "https://kai-beckmann.developerakademie.net/Join-Javascript/smallest_backend_ever"
+  );
   await loadAllTasks();
   loadAllCounters();
   activeSummaryNavLink();
-}
-
-function goToBoard() {
-  window.location.href = "./html/board.html";
 }
