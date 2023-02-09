1e42fa8243ff4ee295dfcef50c8ab9241f487d26
Dennis-Schneider <107464538+Dennis-Schneider@users.noreply.github.com>
2023-02-09 21:38:48 +0100

    AddTask server-connection

---
+++ js/add-task.js
@@ -0,0 +1,18 @@
+async function initAddTask() {
+  await includeHTML();
+  await loadDataFromServer();
+  // activeSummaryNavLink();
+}
+
+function saveTask() {
+  const title = document.getElementById("task-title").value;
+  const description = document.getElementById("task-description").value;
+  let task = {
+    id: tasks.length,
+    title: title,
+    description: description,
+    state: "ToDo"
+  };
+  tasks.push(task);
+  backend.setItem("tasks", tasks);
+}
