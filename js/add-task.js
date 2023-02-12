b171150fe9c9a47d39f54b47c52d5c2e6d5209fb
Dennis-Schneider <107464538+Dennis-Schneider@users.noreply.github.com>
2023-02-12 21:19:24 +0100

    renamed colors | filled categories with js

--- js/add-task.js
+++ js/add-task.js
@@ -1,6 +1,14 @@
+let categories = [
+  { id: 1, name: "Marketing", color: "var(--orange)" },
+  { id: 2, name: "Sales", color: "var(--light-blue)" },
+  { id: 3, name: "Backoffice", color: "var(--red)" },
+  { id: 4, name: "Product management", color: "var(--magenta)" }
+];
+
 async function initAddTask() {
   await includeHTML();
   await loadDataFromServer();
+  loadCategories();
   // activeSummaryNavLink();
 }
 
@@ -20,4 +28,29 @@ function saveTask() {
 
 function loadDelay() {
   window.location.assign("./board.html");
+}
+
+function loadCategories() {
+  let listItems = document.getElementById("category-list");
+  for (let i = 0; i < categories.length; i++) {
+    let listItem = categories[i];
+    listItems.innerHTML += `
+      <div id='category-${listItem["id"]}' onclick='selectCategory(${
+      listItem["id"] - 1
+    })' class="dropdown-item flex">
+        <span class="category-name">${listItem["name"]}</span>
+        <div class="category-color" style="background-color: ${
+          listItem["color"]
+        }"></div>
+      </div>`;
+  }
+}
+
+function selectCategory(id) {
+  document.getElementById("filled-category").innerHTML = `
+      <div class="flex">
+        <span>${categories[id]["name"]}</span>
+        <div class="category-color" style="background-color: ${categories[id]["color"]}"></div>
+      </div>`;
+  document.getElementById("category-input").classList.add("display-none");
 }
