da62749b2f8472b5c007d024224c77db6b8ad180
Dennis-Schneider <107464538+Dennis-Schneider@users.noreply.github.com>
2023-02-15 22:01:43 +0100

    addTask category...

--- js/add-task.js
+++ js/add-task.js
@@ -32,6 +32,7 @@ function loadDelay() {
 
 function loadCategories() {
   let listItems = document.getElementById("category-list");
+  listItems.innerHTML = "";
   for (let i = 0; i < categories.length; i++) {
     let listItem = categories[i];
     listItems.innerHTML += `
@@ -48,11 +49,7 @@ function loadCategories() {
 
 function selectCategory(id) {
   let filledCategory = document.getElementById("filled-category");
-  filledCategory.innerHTML = `
-      <div class="flex">
-        <span>${categories[id]["name"]}</span>
-        <div class="category-color" style="background-color: ${categories[id]["color"]}"></div>
-      </div>`;
+  filledCategory.innerHTML = fillCatergory(id);
   filledCategory.classList.remove("display-none");
   document.getElementById("category-input").value = categories[id]["name"];
   document.getElementById("category-input").classList.add("display-none");
@@ -74,6 +71,61 @@ function newCategory() {
     .getElementById("new-category-colors")
     .classList.remove("display-none");
   document.getElementById("category-input").classList.remove("display-none");
+  document.getElementById("dropdown-accept").classList.remove("display-none");
+  document
+    .getElementById("category-dropdown-arrow")
+    .classList.add("display-none");
+
+  document.getElementById("category-input").value = "";
+  document.getElementById("category-input").select();
   document.getElementById("filled-category").classList.add("display-none");
   toggleDropdown();
+}
+
+function clearInput() {
+  document.getElementById("new-category-colors").classList.add("display-none");
+  document.getElementById("category-input").classList.add("display-none");
+  document.getElementById("dropdown-accept").classList.add("display-none");
+  document
+    .getElementById("category-dropdown-arrow")
+    .classList.remove("display-none");
+
+  document.getElementById("filled-category").classList.remove("display-none");
+  toggleDropdown();
+}
+
+function saveNewCategory() {
+  let newCatValue = document.getElementById("category-input").value;
+  let newCatColor =
+    document.getElementById("new-category-color").style.backgroundColor;
+  categories.push({
+    id: categories.length + 1,
+    name: newCatValue,
+    color: newCatColor
+  });
+  loadCategories();
+  let filledCategory = document.getElementById("filled-category");
+  filledCategory.innerHTML = fillCatergory(categories.length - 1);
+  filledCategory.classList.remove("display-none");
+  document.getElementById("category-input").classList.add("display-none");
+  document.getElementById("new-category-colors").classList.add("display-none");
+  document.getElementById("dropdown-accept").classList.add("display-none");
+  document
+    .getElementById("category-dropdown-arrow")
+    .classList.remove("display-none");
+}
+
+function addNewCatergoryColor(color) {
+  let selectedColor = color;
+  document.getElementById(
+    "new-category-color"
+  ).style.backgroundColor = `var(${selectedColor}`;
+}
+
+function fillCatergory(id) {
+  return `
+<div class="flex">
+  <span>${categories[id]["name"]}</span>
+  <div class="category-color" style="background-color: ${categories[id]["color"]}"></div>
+</div>`;
 }
