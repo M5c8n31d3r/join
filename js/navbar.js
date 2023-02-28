d2f83f043d0e644842ec4f95364a793d9c601a85
Kay Beckmann <info@beckmann-md.de>
2023-02-28 14:48:27 +0100

    setActive in Script.js verschoben

--- js/navbar.js
+++
@@ -1,5 +0,0 @@
-function setActive(id) {
-  let activeItem = document.getElementById(id);
-
-  activeItem.classList.add("nav-item-active");
-}
--- js/script.js
+++ js/script.js
@@ -74,3 +74,9 @@ function getUserExist(email) {
     return false;
   }
 }
+
+function setActive(id) {
+  let activeItem = document.getElementById(id);
+
+  activeItem.classList.add("nav-item-active");
+}
