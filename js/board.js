86f793381afba455bf5006a6c769523542de60fe
Kay Beckmann <info@beckmann-md.de>
2023-03-10 20:11:53 +0100

    added comment

--- js/board.js
+++ js/board.js
@@ -91,6 +91,11 @@ function renderCard(task) {
     </div>`;
 }
 
+/**
+ *
+ * @param {OBJECT} subtasks Array of subtasks
+ * @returns {INT} Counter of subtasks, which are ready
+ */
 function countDoneSubtasks(subtasks) {
   let countDone = 0;
   for (let i = 0; i < subtasks.length; i++) {
